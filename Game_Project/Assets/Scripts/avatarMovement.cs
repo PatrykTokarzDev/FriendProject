fd7edc142b3c5c6da1a0989610fe16f1edc1d37a
PatrykTokarzDev <78189369+PatrykTokarzDev@users.noreply.github.com>
2021-03-19 21:02:42 -0400

    Movement added
    add movement

---
+++ Game_Project/Assets/Scripts/avatarMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avatarMovement : MonoBehaviour
+{
+    //variable declaration
+    private float speed = 10;
+    public float jumpHeight = 5;
+    Rigidbody2D rb;
+
+
+
+    /// <summary>
+    /// Gets the rigidBody component of the avatar sprite
+    /// </summary>
+    private void Awake()
+    {
+
+
+        rb = transform.GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Gets the input controls 
+    /// adds momentum when pressing A,D or Left arrow, right arrow
+    /// space key increases height
+    /// </summary>
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, 0);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpHeight;
+        }
+
+
+    }
+}
