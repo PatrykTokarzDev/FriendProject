4595f7003f3cbceedce55261617525b3e6b59aa4
PatrykTokarzDev <78189369+PatrykTokarzDev@users.noreply.github.com>
2021-05-04 19:32:37 -0400

    Added three different movement options
    three options total, two are commented out. If you have the time please look at all three and see if there are any you like. I personally like option 2.
    Also changed sprite to cuphead XD because it fits the dimensions of our dragon

--- Game_Project/Assets/Scripts/avatarMovement.cs
+++ Game_Project/Assets/Scripts/avatarMovement.cs
@@ -6,12 +6,13 @@ public class avatarMovement : MonoBehaviour
 {
 
     //variable declaration
-    public Vector2 speed2 = new Vector2(30, 30);
-    private float speed = 10;
-    public float jumpHeight = 50;
+    private Vector2 speed2 = new Vector2(30, 30);
+    private float speed = 5f;
+    private float jumpHeight = 50;
     Rigidbody2D rb;
 
-
+    private float MovementSmoothing = 0.001f; //FOR OPTION 2 THIS VARIABLE IS WHAT TO CHANGE TO ADD MOMENTUM, THE LOWER THE NUMBER THE LESS MOMENTUM BUT DO NOT PUT IT TO 0 OR ABOVE 1 
+    private Vector3 velMove = Vector3.zero;
 
     /// <summary>
     /// Gets the rigidBody component of the avatar sprite
@@ -32,21 +33,63 @@ public class avatarMovement : MonoBehaviour
     void Update()
     {
 
+
+        ///
+        /// OPTION 1
+        ///
+
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        //transform.position += move * Time.deltaTime * speed;
+
+
+
+        ///
+        /// OPTION 2
+        ///
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        Vector3 targetVelocity = new Vector2(horizontal * 10f, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velMove, MovementSmoothing);
 
-        Vector2 movement = new Vector2(horizontal*speed2.x, 0);
 
-        //rb.AddForce(movement * speed);
 
-        movement *= Time.deltaTime;
-        transform.Translate(movement);
 
-        if (Input.GetAxis("Jump") > 0)
-        {
-            transform.Translate(Vector2.up * jumpHeight * Time.deltaTime, Space.World);
-        }
+
+
+
+
+
+
+
+
+
+        ///
+        /// OPTION 3
+        ///
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(horizontal*speed2.x, 0);
+
+        ////rb.AddForce(movement * speed);
+
+        //movement *= Time.deltaTime;
+        ////transform.Translate(movement);
+
+        //transform.position += movement * (Time.deltaTime * 5);
+        //if (Input.GetAxis("Jump") > 0)
+        //{
+        //    transform.Translate(Vector2.up * jumpHeight * Time.deltaTime, Space.World);
+        //}
 
 
     }
+
+
+
+
+
+
+
+
 }
