2f18523d6ff1276bbd9a3fd6de077cc0a4fbd6c2
PatrykTokarzDev <78189369+PatrykTokarzDev@users.noreply.github.com>
2021-05-14 19:25:47 -0400

    Movement

---
+++ Game_Project/Assets/CharacterController2D.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 9;
+
+    [SerializeField]
+    float walkAcceleration = 75;
+
+    [SerializeField]
+    float airAcceleration = 30;
+
+    [SerializeField]
+    float groundDeceleration = 70;
+
+    [SerializeField]
+    float jumpHeight = 4;
+
+    private BoxCollider2D boxCollider;
+
+    private Vector2 velocity;
+
+
+    private bool grounded;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+      
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (grounded)
+        {
+            velocity.y = 0;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+            
+                velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+            }
+        }
+
+        float acceleration = grounded ? walkAcceleration : airAcceleration;
+        float deceleration = grounded ? groundDeceleration : 0;
+
+        if (moveInput != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+
+        transform.Translate(velocity * Time.deltaTime);
+
+        grounded = false;
+
+       
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+
+        foreach (Collider2D hit in hits)
+        {
+            // Ignore our own collider.
+            if (hit == boxCollider)
+                continue;
+
+            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+
+                if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && velocity.y < 0)
+                {
+                    grounded = true;
+                }
+            }
+        }
+    }
+}
