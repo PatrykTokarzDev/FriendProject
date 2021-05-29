395e2b4f98ffa4e18d8fd2bffbdb1f33b7751a48
PatrykTokarzDev <78189369+PatrykTokarzDev@users.noreply.github.com>
2021-05-28 20:17:35 -0400

    Dash

--- Game_Project/Assets/Dash.cs
+++ Game_Project/Assets/Dash.cs
@@ -15,7 +15,7 @@ public class Dash : MonoBehaviour
     private float dashTime;
     private float startDashTime;
     private int direction;
-   
+
 
 
     private void Start()
@@ -23,33 +23,36 @@ public class Dash : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         //rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.None;
         dashTime = startDashTime;
-            
+
     }
 
-    void Update () 
-     {
+    void Update()
+    {
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             StartCoroutine(dashCoolDown());
         }
-       
+
     }
 
 
     private IEnumerator dashCoolDown()
     {
-        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-        rb.velocity = Vector2.left * dashSpeed;
-        yield return new WaitForSeconds(1);
-        rb.velocity = Vector2.zero;
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-
+       // rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        //rb.velocity = Vector2.left * dashSpeed;
+        rb.gravityScale = 0f;
+        rb.AddForce(Vector3.left * 10000);
+        yield return new WaitForSeconds(0.1f);
+        rb.velocity = Vector3.zero;
+        rb.gravityScale = 1f;
+        //rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        
 
 
         //PROBLEM: when the player uses this script the Y or vertical does not lock. Sometimes the player also stops moving completely
     }
- }
+}
  
  
 
